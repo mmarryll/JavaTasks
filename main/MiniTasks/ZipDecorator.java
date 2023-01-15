6c638c533e98d52eb5ce8d01ca7c5fb721fdf451
Mariya Lagun <108588991+mmarryll@users.noreply.github.com>
2023-01-15 15:10:47 +0300

    Update and rename ZipperDecorator.java to ZipDecorator.java

--- main/MiniTasks/ZipDecorator.java
+++ main/MiniTasks/ZipDecorator.java
@@ -1,39 +1,74 @@
-package common.MiniTasks;
+package MiniTasks;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
-public class ZipperDecorator extends DataDecorator {
-    public ZipperDecorator(IData data) {
+public class ZipDecorator extends DataDecorator {
+    public ZipDecorator(IData data) {
         super(data);
     }
 
-    @Override
+    public void write(String filename, String text) {
+        super.write(filename, text);
+
+        try {
+            this.zip(filename);
+        } catch (FileNotFoundException var4) {
+            var4.printStackTrace();
+        } catch (IOException var5) {
+            var5.printStackTrace();
+        }
+
+    }
     public String read(String filename) {
         try {
-            unzip(filename + ".zip");
-        } catch (IOException e) {
-            e.printStackTrace();
+            this.unzip(filename + ".zip");
+        } catch (IOException var3) {
+            var3.printStackTrace();
         }
+
         return super.read(filename);
     }
 
-    @Override
-    public void write(String filename, String text) {
-        super.write(filename, text);
-        try {
-            zip(filename);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+    private void unzip(String filename) throws IOException {
+        File destDir = new File("./");
+        byte[] buffer = new byte[1024];
+        ZipInputStream zis = new ZipInputStream(new FileInputStream(filename));
+
+        for(ZipEntry zipEntry = zis.getNextEntry(); zipEntry != null; zipEntry = zis.getNextEntry()) {
+            File newFile = newFile(destDir, zipEntry);
+            if (zipEntry.isDirectory()) {
+                if (!newFile.isDirectory() && !newFile.mkdirs()) {
+                    throw new IOException("Failed to create directory " + newFile);
+                }
+            } else {
+                File parent = newFile.getParentFile();
+                if (!parent.isDirectory() && !parent.mkdirs()) {
+                    throw new IOException("Failed to create directory " + parent);
+                }
+
+                FileOutputStream fos = new FileOutputStream(newFile);
+
+                int len;
+                while((len = zis.read(buffer)) > 0) {
+                    fos.write(buffer, 0, len);
+                }
+
+                fos.close();
+            }
         }
+
+        zis.closeEntry();
+        zis.close();
     }
 
     private void zip(String filename) throws IOException {
-
         FileOutputStream fos = new FileOutputStream(filename + ".zip");
         ZipOutputStream zipOut = new ZipOutputStream(fos);
         File fileToZip = new File(filename);
@@ -41,59 +76,27 @@ public class ZipperDecorator extends DataDecorator {
         ZipEntry zipEntry = new ZipEntry(fileToZip.getName());
         zipOut.putNextEntry(zipEntry);
         byte[] bytes = new byte[1024];
+
         int length;
-        while ((length = fis.read(bytes)) >= 0) {
+        while((length = fis.read(bytes)) >= 0) {
             zipOut.write(bytes, 0, length);
         }
+
         zipOut.close();
         fis.close();
         fos.close();
     }
 
-    private void unzip(String filename) throws IOException {
-        File destDir = new File("./");
-        byte[] buffer = new byte[1024];
-        ZipInputStream zis = new ZipInputStream(new FileInputStream(filename));
-        ZipEntry zipEntry = zis.getNextEntry();
-        while (zipEntry != null) {
-            File newFile = newFile(destDir, zipEntry);
-            if (zipEntry.isDirectory()) {
-                if (!newFile.isDirectory() && !newFile.mkdirs()) {
-                    throw new IOException("Failed to create directory " + newFile);
-                }
-            } else {
-                // fix for Windows-created archives
-                File parent = newFile.getParentFile();
-                if (!parent.isDirectory() && !parent.mkdirs()) {
-                    throw new IOException("Failed to create directory " + parent);
-                }
-
-                // write file content
-                FileOutputStream fos = new FileOutputStream(newFile);
-                int len;
-                while ((len = zis.read(buffer)) > 0) {
-                    fos.write(buffer, 0, len);
-                }
-                fos.close();
-            }
-            zipEntry = zis.getNextEntry();
-        }
-        zis.closeEntry();
-        zis.close();
-    }
 
     public static File newFile(File destinationDir, ZipEntry zipEntry) throws IOException {
         File destFile = new File(destinationDir, zipEntry.getName());
-
         String destDirPath = destinationDir.getCanonicalPath();
         String destFilePath = destFile.getCanonicalPath();
-
         if (!destFilePath.startsWith(destDirPath + File.separator)) {
             throw new IOException("Entry is outside of the target dir: " + zipEntry.getName());
+        } else {
+            return destFile;
         }
-
-        return destFile;
     }
-
-
 }
+
