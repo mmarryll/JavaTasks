256b6c1fcbc918a137c0f0fc20fc39dfd8ee552d
Mariya Lagun <108588991+mmarryll@users.noreply.github.com>
2023-01-15 15:19:12 +0300

    Update

--- GUI/Java/Main.java
+++ GUI/Java/Main.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("sample.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("gui.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        stage.setTitle("Convert");
+        stage.setTitle("MiniTasks");
         stage.setScene(scene);
         stage.show();
     }
