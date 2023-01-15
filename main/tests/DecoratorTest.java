e5ecb29b57ea24ad8ecd3901fb0c0ce6ddec7c3d
Mariya Lagun <108588991+mmarryll@users.noreply.github.com>
2023-01-15 15:28:39 +0300

    Update DecoratorTest.java

--- main/tests/DecoratorTest.java
+++ main/tests/DecoratorTest.java
@@ -1,48 +1,47 @@
-package test;
+package tests;
 
-import com.company.*;
+import MiniTasks.CompressDecorator;
+import MiniTasks.EncryptionDecorator;
+import MiniTasks.ReadWrite;
+import MiniTasks.ZipDecorator;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
-import com.company.ReadWrite;
-import org.junit.Assert;
-import org.junit.Test;
-
-public class DecoratorTest {
-    @Test
-    public void zipUnzip(){
-        String path = "test files/test1.txt";
-        ReadWrite readWrite = new ReadWrite();
-        ZipperDecorator zipperDecorator = new ZipperDecorator(readWrite);
-        String text1 = readWrite.read(path);
-        zipperDecorator.write("test1.zip", text1);
-        String text2 = zipperDecorator.read("test1.zip");
-
-        Assert.assertEquals(text1, text2);
-
+public class DecoratorTests {
+    public DecoratorTests() {
     }
 
     @Test
-    public void encodeDecode(){
+    public void zipUnzip() {
+        String path = "test files/test1.txt";
+        ReadWrite readWrite = new ReadWrite();
+        ZipDecorator zipperDecorator = new ZipDecorator(readWrite);
+        String text1 = readWrite.read(path);
+        zipperDecorator.write("test1.zip", text1);
+        String text2 = zipperDecorator.read("test1.zip");
+        Assert.assertEquals(text1, text2);
+    }
+
+    @Test
+    public void encodeDecode() {
         String path = "test files/test1.txt";
         ReadWrite readWrite = new ReadWrite();
         EncryptionDecorator encryptionDecorator = new EncryptionDecorator(readWrite);
         String text1 = readWrite.read(path);
         encryptionDecorator.write("test1.zip", text1);
         String text2 = encryptionDecorator.read("test1.zip");
-
         Assert.assertEquals(text1, text2);
-
     }
 
     @Test
-    public void compressDecompress(){
+    public void compressDecompress() {
         String path = "test files/test1.txt";
         ReadWrite readWrite = new ReadWrite();
         CompressDecorator compressDecorator = new CompressDecorator(readWrite);
         String text1 = readWrite.read(path);
         compressDecorator.write("test1.zip", text1);
         String text2 = compressDecorator.read("test1.zip");
-
         Assert.assertEquals(text1, text2);
-
     }
 }
+
